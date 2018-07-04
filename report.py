7698714b6b65c77ef19a30f3d08bca2ba8102e3d
Matt L <mattlipski5543@gmail.com>
2018-07-04 20:20:20 +0100

    Fixed error report, PEP8 compatible

--- report.py
+++ report.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Imports
 import psycopg2
 import datetime
@@ -7,9 +9,10 @@ db = psycopg2.connect("dbname=news")
 
 # Create a cursor, use this to perform queries
 cursor = db.cursor()
-file = open("log.txt","w")
+file = open("log.txt", "w")
 time = datetime.datetime.now()
 file.write("--- New Report: " + str(time) + " ---")
+
 
 def reportArticles():
     print "Getting top 3 Articles..."
@@ -18,7 +21,7 @@ def reportArticles():
 
     # Query to execute
     topArticlesQuery = """
-    SELECT a.title, COUNT(l.path) AS views
+    SELECT a.title, COUNT(*) AS views
     FROM articles a
     JOIN log l ON l.path LIKE '%' || a.slug || '%'
     GROUP BY a.title
@@ -28,45 +31,45 @@ def reportArticles():
     cursor.execute(topArticlesQuery)  # Execute the query
     topArticles = cursor.fetchall()  # Fetch all results into variable
     for a in topArticles:
-        # For each result, print a line of text to report the number of views.
-        file.write("Article \"" + a[0] + "\" reached: " + str(a[1]) + " views.\n")
+        # A line of text for each result
+        file.write("Article \"" + a[0] + "\": " + str(a[1]) + " views.\n")
     print "Done."
 
 
 def reportAuthors():
-    print "Getting top 3 authors..."
+    print "Getting top 4 authors..."
 
-    file.write("\nTop 3 Authors by Views:\n")
+    file.write("\nTop 4 Authors by Views:\n")
 
     # Query to execute
     topAuthorsQuery = """
-    SELECT au.name, count(ar.slug) as views
+    SELECT au.name, count(*) as views
     FROM authors au
     LEFT JOIN articles ar ON au.id = ar.author
     JOIN log l ON l.path LIKE '%' || ar.slug || '%'
     GROUP BY au.name
     ORDER BY views DESC
-    LIMIT 3
+    LIMIT 4
     """
     cursor.execute(topAuthorsQuery)  # Execute the query
     topAuthors = cursor.fetchall()  # Fetch all results into variable
     for a in topAuthors:
         # For each result, print a line of text.
-        file.write("Author \"" + a[0] + "\" reached " + str(a[1]) + " views.\n")
+        file.write("Author \"" + a[0] + "\": " + str(a[1]) + " views.\n")
     print "Done."
 
 
 def reportLogErrors():
     print "Getting error logs..."
 
-    file.write("\nDays on which more than 1 percent of requests were errors:\n")
+    file.write("\nDays on which more than 1 percent of requests had errors:\n")
 
     # Get Log
     logQuery = """
-    SELECT date_trunc('day', l.time) AS day,
-    COUNT(l.status) AS total,
+    SELECT cast(l.time as date) AS day,
+    COUNT(*) AS total,
     l2.errors AS errors,
-    ((l2.errors / COUNT(l.status)) * 100) AS error_perc
+    ROUND((l2.errors * 100) / COUNT(*)::numeric, 3) AS error_perc
     FROM log l
     LEFT JOIN (
         SELECT COUNT(l2.status) AS errors,
@@ -76,13 +79,13 @@ def reportLogErrors():
         GROUP BY error_day
         ) l2 ON date_trunc('day', l.time) = l2.error_day
     GROUP BY day, l2.errors
-    HAVING ((l2.errors / COUNT(l.status)) * 100) >= 1
+    HAVING ROUND((l2.errors * 100) / COUNT(*), 3) >= 1
     """
     cursor.execute(logQuery)
     logs = cursor.fetchall()
     if len(logs) > 0:
         for log in logs:
-            file.write(str(log[0]) + ": " + str(log[3]) + "\n")
+            file.write(str(log[0]) + ": " + str(log[3]) + "%\n")
     else:
         file.write("No days had more than 1 percent errors")
     print "Done."
